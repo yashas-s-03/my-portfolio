a87bfd05f92f702d3b131f484db41cc67f44fae2
yashas-s-03 <rabc211@gmail.com>
2025-04-06 22:56:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-// Load particles.js config
-particlesJS.load('particles-js', 'particles.json', function() {
-    console.log('particles.js config loaded');
-  });
-  
-  // Typing effect
-  new Typed('#typed-text', {
-    strings: [
-      "I like to listen",
-      "I like to build",
-      "I like to code",
-      "I like to play"
-    ],
-    typeSpeed: 60,
-    backSpeed: 40,
-    loop: true
-  });
+
   particlesJS("particles-js", {
   particles: {
     number: {
